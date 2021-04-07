59162d6ee1182258c9424ccf4f476e04ef5edcc7
tkenned2020 <tkenned2020@outlook.com>
2021-04-07 17:51:51 -0400

    planning

--- connect-four.js
+++ connect-four.js
@@ -1,10 +1,93 @@
-/* 
+/*
 Understanding:
+list: create code indicate when col. is full
+      indicating which turn for play by utilizing
+      by using click-targets
 
-Plan:
+putting a token in a square
 
+Plan: when col. is full set class to 'full'
+    on specific col.
+    ex. when col.length === 6 set class to 'full'
 
+    add black or red class to the div with the
+    the 'click-targets" id
+
+when placing token in square:
+create div that will indicate what color and then
+make this div the child of that square
 
 
 Player turns utilize show and hide method
 */
+window.addEventListener('DOMContentLoaded', event => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+});
